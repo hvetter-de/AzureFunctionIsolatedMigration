460fa578376453d5e04bbce36717d4a46de22587
Holger Vetter <hvetter@daenet.com>
2023-08-09 17:12:33 +0200

    migrate to isolated function

--- src/AzureFunctionIsolatedMigration/Function1.cs
+++ src/AzureFunctionIsolatedMigration/Function1.cs
@@ -2,22 +2,29 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
 
 namespace AzureFunctionIsolatedMigration
 {
-    public static class Function1
+    public class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private readonly ILogger _logger;
+
+        public Function1(ILogger<Function1> logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
@@ -29,7 +36,10 @@ namespace AzureFunctionIsolatedMigration
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            HttpResponseData responseData = req.CreateResponse(HttpStatusCode.OK);
+            responseData.WriteString(responseMessage);
+
+            return responseData;
         }
     }
 }
---
+++ src/AzureFunctionIsolatedMigration/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionIsolatedMigration
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+                })
+                .Build();
+            await host.RunAsync();
+        }
+    }
+}
